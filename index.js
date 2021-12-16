e204cbbc34919155f4a05234bb46318f88abfc53
Fernanda Barbosa <fernanda.b.santos@outlook.com.br>
2021-12-15 21:02:09 -0300

    Alterações da segunda aula

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-function carregamentoPagina() {
-    alert("Mensagem apresentada no carregamento do sistema");
-}
+// function carregamentoPagina() {
+//     alert("Mensagem apresentada no carregamento do sistema");
+// }
+
+$(() => {
+    //Código executado no carregamento da página
+    // alert("Mensagem apresentada no carregamento do sistema");
+    $("#btnsalvar").click(() =>){
+        console.log("FUNÇÃO CLICK FUNCIONAOU")
+    }
+})
